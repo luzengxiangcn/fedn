a11a9603292202636254dc15a5a1c10182e371f6
luzengxiang <luzengxiang@enn.cn>
2021-09-14 09:53:13 +0800

    client shell debug

--- local_run/shell_script/5_run_client.sh
+++ local_run/shell_script/5_run_client.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$PYTHONPATH:../python/
 echo $PYTHONPATH
-python ../python/cli/run/client.py  -n client --init=../config/settings-reducer.yaml.template
+python ../python/cli/run/client.py  -n client --init=../config/settings-client.yaml.template
